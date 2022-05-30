7174ac5b5aa577f99c53dd8e3672e332446c76d2
Семен Севастеев <kong01@bk.ru>
2022-05-30 12:27:27 +0800

    Создание fxml и окна авторизации

---
+++ src/main/java/com/example/kursovaya/ControllAuth.java
@@ -0,0 +1,33 @@
+package com.example.kursovaya;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ControllAuth {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authSign;
+
+    @FXML
+    private TextField logField;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
